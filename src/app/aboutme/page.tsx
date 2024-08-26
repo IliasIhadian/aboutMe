2d7db168d7e76b17053b3dc113a2b22e8ad9f3bb
Ilias Ihadian <i.ihadian@gmail.com>
2024-08-26 12:09:08 +0200

    finished projects

--- src/app/aboutme/page.tsx
+++ src/app/aboutme/page.tsx
@@ -9,8 +9,15 @@ export default function aboutme() {
   return (
     <main className="rounded p-8">
       <AspectRatio ratio={16 / 9} className="	rounded-md border-2 border-black">
-        <Hero></Hero>
-        <Sidebar animate={"a"}></Sidebar>
+        <div className="grid grid-cols-3 gap-4 ">
+          <div className="order-first col-span-3 ">
+            <Hero></Hero>
+          </div>
+          <div className="">
+            <Sidebar animate={"a"}></Sidebar>
+          </div>
+          <div className="col-span-2"></div>
+        </div>
       </AspectRatio>
     </main>
   );
