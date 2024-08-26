2a3ed597bf2deea13a289ee05d20db91535ce9a2
Ilias Ihadian <i.ihadian@gmail.com>
2024-08-26 17:22:14 +0200

    bc of build

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-export default function Sidebar({ animate }) {
+export default function Sidebar({ animate }: { animate: any }) {
   return (
     <>
       <aside className="px-8">
